c881f06289e69644b4b8de33dd09569a9a4c1441
Sergio <sergioruza22@gmail.com>
2023-04-27 15:15:03 -0300

    chore: [200~implements the function to read the csv file

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,22 +1,13 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    with open(path, encoding="utf-8") as file:
+        data = csv.DictReader(file)
+        return [row for row in data]
 
 
 def get_unique_job_types(path: str) -> List[str]:
@@ -53,3 +44,6 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
         List of jobs with provided job_type
     """
     raise NotImplementedError
+
+
+read("data/jobs.csv")
