cd0d25ba2624420782bfd5b6e5ef8f5bcf23b7f3
Sergio <sergioruza22@gmail.com>
2023-04-27 15:27:39 -0300

    chore: implements function that returns all job types

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -11,21 +11,11 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    data = read(path)
+    filter_jobs = set()
+    for job in data:
+        filter_jobs.add(job["job_type"])
+    return filter_jobs
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
