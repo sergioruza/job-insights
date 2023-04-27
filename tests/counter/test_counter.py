db0cb7219d1f75dafb1f30b690da53438e293b43
Sergio <sergioruza22@gmail.com>
2023-04-27 19:43:26 -0300

    test: counter words

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,4 +2,9 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    result = count_ocurrences("data/jobs.csv")
+    result = count_ocurrences("data/jobs.csv", "python")
+    assert result == 1639
+
+
+# js - 122
+# python 1639
