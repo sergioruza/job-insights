00ff2158dd81d329e9b6e6f53a6f114459e59c12
Sergio <sergioruza22@gmail.com>
2023-04-27 18:04:44 -0300

    feat: filter by salary - [incomplete]

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -23,33 +23,37 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    verify_exists = "min_salary" not in job and "max_salary" not in job
-    verify_num = job["min_salary"] < job["max_salary"]
-    verify_type_num = isinstance(job["min_salary"], int)
-    if verify_exists and verify_num and verify_type_num:
-        raise ValueError("seila")
+    try:
+        verify_exists = "min_salary" not in job and "max_salary" not in job
+        verify_num = job["min_salary"] < job["max_salary"]
+        verify_type_num = isinstance(job["min_salary"], int)
+        if verify_exists and verify_num and verify_type_num:
+            raise ValueError
 
-    if salary < job["max_salary"] and salary > job["min_salary"]:
-        return True
-    else:
-        return False
+        if salary < job["max_salary"] and salary > job["min_salary"]:
+            return True
+        else:
+            return False
+    except ValueError:
+        print("erro aqui hein")
+        raise ValueError
 
 
 def filter_by_salary_range(
     jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
+    list_jobs = []
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
+    for job in jobs:
+        if isinstance(job["min_salary"], int) and isinstance(
+            job["max_salary"], int
+        ):
+            job = {
+                "max_salary": int(job["max_salary"]),
+                "min_salary": int(job["min_salary"]),
+            }
 
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+            if matches_salary_range(job, salary):
+                list_jobs.append(job)
+
+    return list_jobs
