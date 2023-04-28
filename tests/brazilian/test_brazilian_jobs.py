2d518db1ff08b118767959eff13926a70c496529
Sergio <sergioruza22@gmail.com>
2023-04-28 07:31:40 -0300

    remove print()

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -4,5 +4,4 @@ from tests.mocks.english_jobs import mock_english
 
 def test_brazilian_jobs():
     result = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
-    print(mock_english)
     assert result == mock_english
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,6 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    result = sort_by("data/jobs.csv", "min_salary")
+    print("aquiii", result)
