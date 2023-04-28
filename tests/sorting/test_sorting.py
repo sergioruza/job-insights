5d0891e01d2ac5a91d4b03c3234f6148bc7ab517
Sergio <sergioruza22@gmail.com>
2023-04-28 08:08:36 -0300

    error importing mock

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,6 +1,9 @@
 from src.pre_built.sorting import sort_by
 
+from tests.sorting.mocks.sort_mock import jobs_mock
+
+print(jobs_mock)
+
 
 def test_sort_by_criteria():
-    result = sort_by("data/jobs.csv", "min_salary")
-    print("aquiii", result)
+    pass
