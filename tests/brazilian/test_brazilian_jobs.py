10aa7f1ce8b014bbc439f6b8c6105be40f1a3ea6
Sergio <sergioruza22@gmail.com>
2023-04-28 07:12:34 -0300

    test: read brazilian jobs traslate

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+from tests.mocks.english_jobs import mock_english
 
 
 def test_brazilian_jobs():
-    pass
+    result = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    print(mock_english)
+    assert result == mock_english
